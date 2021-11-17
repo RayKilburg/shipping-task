410fb8b891fdbb66145869b8c48355e37117439d
RayKilburg <rkilburg42@gmail.com>
2021-11-16 23:37:11 -0500

    packageSchema

--- models/Package.js
+++ models/Package.js
@@ -6,7 +6,15 @@ const mongoose = require('mongoose');
 
 const PackageSchema = new mongoose.Schema({
   // TODO: Complete this according the the specifications outlined in the README
+  itemName: {
+    type: String,
+    required: true,
+  },
+  itemContents: {
+    type: Number,
+    required: true,
+  },
 });
- 
+
 const PackageModel = mongoose.model('package', PackageSchema);
 module.exports = PackageModel;
