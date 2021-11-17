15c9d32ad49b2a01221354e38a4038e2071c2b38
RayKilburg <rkilburg42@gmail.com>
2021-11-16 23:54:26 -0500

    shipment schema

--- models/Shipment.js
+++ models/Shipment.js
@@ -6,6 +6,14 @@ const mongoose = require('mongoose');
 
 const ShipmentSchema = new mongoose.Schema({
   // TODO: Complete this according the the specifications outlined in the README
+  shipmentName: {
+    type: String,
+    required: true,
+  },
+  shipmentContents: {
+    type: Number,
+    required: false,
+  },
 });
 
 const ShipmentModel = mongoose.model('shipment', ShipmentSchema);
